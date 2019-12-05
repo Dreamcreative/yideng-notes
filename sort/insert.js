a343eefdf40847b4058f81774195306a238313da
马啸 <m543438924@163.com>
2019-12-06 00:52:36 +0800

    新增insert/select排序

---
+++ sort/insert.js
@@ -0,0 +1,30 @@
+class Insert {
+  constructor(data) {
+    this.data = data;
+  }
+  index() {
+    let length = this.data.length;
+    let temp, inner;
+    /**
+     * 外层循环：从第二个值开始遍历
+     * temp用来存储第二个值
+     * inner用来存储第二个值的位置
+     */
+    for (let outer = 1; outer < length; outer++) {
+      inner = outer;
+      temp = this.data[outer];
+      /**
+       * 内部循环：判断第二个值小于等于它前面位置的值
+       * 如果小于，则将前面位置的值赋值给后一位置的值，同时将位置inner进行--，使得位置向前移动
+       */
+      while (inner > 0 && this.data[inner - 1] >= temp) {
+        this.data[inner] = this.data[inner - 1];
+        inner--;
+      }
+      this.data[inner] = temp;
+    }
+  }
+}
+var insert = new Insert([8, 9, 7, 6, 5, 4, 3, 2, 1]);
+insert.index();
+console.log(insert.data);
---
+++ sort/select.js
@@ -0,0 +1,34 @@
+class Select {
+  constructor(data) {
+    this.data = data;
+  }
+  swap(target, index1, index2) {
+    let temp = target[index1];
+    target[index1] = target[index2];
+    target[index2] = temp;
+  }
+  index() {
+    let a = 1;
+    let length = this.data.length;
+    for (let outer = 0; outer < length - 1; ++outer) {
+      // 记录最小值的位置
+      let min = outer;
+      /**
+       * 内部循环：从第一个位置开始
+       * 当找到一个比当前值小的值时，将小值的位置赋值给min
+       * 下次内部循环会拿着新的小值去比较
+       * 最后当数组遍历完时，将拿到的最小值的与当前位置的值进行交换
+       */
+      for (let inner = outer + 1; inner < length; ++inner) {
+        if (this.data[inner] < this.data[min]) {
+          min = inner;
+        }
+        console.log(a++);
+      }
+      this.swap(this.data, outer, min);
+    }
+  }
+}
+var select = new Select([8, 9, 7, 6, 5, 4, 3, 2, 1]);
+select.index();
+console.log(select.data);
